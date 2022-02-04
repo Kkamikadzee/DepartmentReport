f064e8a822be5eb3384d483438dd2cf358546c81
Kkamikadzee <Kkamikadzee@yandex.ru>
2022-02-04 17:35:57 +0300

    Добавил индексацию по имени столбца.

--- TemplateDocEditor/TableCells.cs
+++ TemplateDocEditor/TableCells.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using DepartmentReportGenerator.DocEditor;
+using DepartmentReportGenerator.Extensions;
 using Word = Microsoft.Office.Interop.Word;
 
 namespace TemplateDocEditor
@@ -11,14 +13,32 @@ namespace TemplateDocEditor
         
         public string this[int row, int column]
         {
-            get => _table.Cell(_docTable.HasHeader ? row + 2 : row, column + 1).Range.Text;
-            set => _table.Cell(_docTable.HasHeader ? row + 2 : row, column + 1).Range.Text = value;
+            get => _table.Cell(_docTable.HasHeader ? row + 2 : row + 1, column + 1).Range.Text;
+            set => _table.Cell(_docTable.HasHeader ? row + 2 : row + 1, column + 1).Range.Text = value;
+        }
+
+        public string this[int row, string column]
+        {
+            get => _table.Cell(_docTable.HasHeader ? row + 2 : row + 1, GetColumnIndexByName(column) + 1).Range.Text;
+            set => _table.Cell(_docTable.HasHeader ? row + 2 : row + 1, GetColumnIndexByName(column) + 1).Range.Text = value;
         }
 
         public TableCells(Word.Table table, Table docTable)
         {
             _table = table;
             _docTable = docTable;
+        }
+
+        private int GetColumnIndexByName(string columnName)
+        {
+            int index = _docTable.ColumnNames.IndexOf(columnName);
+
+            if (index == -1)
+            {
+                throw new IndexOutOfRangeException($"Column with name {columnName} is not found");
+            }
+
+            return index;
         }
     }
 }
